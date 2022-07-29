f421f2997c81cb3d1b3924545876bf17282c74c1
Shivaraj Umadi <shivaumadi@outlook.com>
2022-07-29 10:25:10 +0530

    28-07-2022

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var task = document.getElementById('btn').addEventListener('click',taskinput);
 
+
 function taskinput(){
     var lin = document.createElement('li');
-    lin.className = 'list-group-item mb-3';
-    // lin.classList.add('mb-2');
+    lin.className = 'list-group-item mb-3 d-flex justify-content-between';
     var input = document.getElementById('inputId').value;
     var text = document.createTextNode(input);
     lin.appendChild(text);
@@ -13,4 +13,18 @@ function taskinput(){
         document.getElementById('list').appendChild(lin);
     }
     document.getElementById("inputId").value = "";
-}
+    var span = document.createElement('button');
+    var txt = document.createTextNode('X');
+    span.className = 'btn btn-danger'
+    span.appendChild(txt);
+    lin.appendChild(span);
+    span.addEventListener('click',function(e){
+        e.preventDefault();
+        // let prompt = prompt('Are you sure?');
+        let propt = prompt('Are you sure?');
+        if(propt=='yes'){
+            var div = this.parentElement;
+            div.remove();
+        }
+    })
+} 
