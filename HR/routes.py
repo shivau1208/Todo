4ad8ac7c6f1ee08468190ae6197cd7fa02a480b8
Shivaraj Umadi <shivau1208@github.com>
2022-08-28 01:10:36 +0530

    27-08/fetch-POST & DB row update

--- HR/routes.py
+++ HR/routes.py
@@ -16,8 +16,19 @@ def task():
         if(task != ''):
             cursor.execute('INSERT INTO tasks (task_name) VALUES(%s)' , (task,))
             cursor.connection.commit()
-        else:
-            return render_template('index.html')
+        # else:
+        #     return render_template('index.html')
+    return render_template('index.html')
+@app.route('/complete', methods=['POST'])
+def comp():
+    cursor = mydb.connection.cursor()
+    if request.method == 'POST':
+        complete = request.get_json()
+        for key,val in complete.items():
+            print(val)
+            cursor.execute('UPDATE tasks SET task_complete="Complete" WHERE task_name=%s' , (val,))
+            cursor.connection.commit()
+    return render_template('index.html')
     
 @app.route('/list', methods=['GET'])
 def list():
@@ -27,15 +38,15 @@ def list():
         data = cursor.fetchall()
         cursor.close()
         return jsonify(data)
+    return render_template('index.html')
 
 @app.route('/del',methods=['POST'])
 def delete():
     cursor = mydb.connection.cursor()
     if request.method == 'POST':
-        nm = request.get_json()
-        print(nm)
-        # name = json.loads(nm.text)
-        # print(name)
+        nm = request.get_json(force=True)
         cursor.execute('DELETE FROM tasks WHERE task_name = %s',(nm,))
         cursor.connection.commit()
+        cursor.close()
+    return render_template('index.html') 
         
--- HR/static/main.js
+++ HR/static/main.js
@@ -1,24 +1,4 @@
-var task = document.getElementById('btn').addEventListener('click',run);
-var dlt = document.getElementById('del').addEventListener('click',det);
-var rgt = document.getElementById('right').addEventListener('click', right)
-
-function right(){
-    var li = document.querySelectorAll('.li');
-    for (j=0; j<li.length;j++){
-        // console.log(li[j].firstChild.style)
-        li[j].firstChild.style = 'text-decoration: line-through;text-decoration-style: solid;text-decoration-thickness: 2px;';
-    }
-}
-
-function det(){
-    let propt = prompt('Are you sure?');
-    if(propt=='yes'){
-        let lis = document.querySelectorAll('.li');
-        for (i=0; i<lis.length;i++){
-            lis[i].remove();
-        }
-    }
-}
+document.getElementById('btn').addEventListener('click',run);
 
 function run(){
     var input = document.getElementById('inputId').value;
@@ -31,11 +11,8 @@ function run(){
 }
 
 function taskinput(data){
-
     var lin = document.createElement('li');
     lin.className = 'list-group-item mt-3 d-flex li';
-    // var input = data;
-    // inp(lin);
     var tsk = document.createTextNode(data);
     var text = document.createElement('text');
     text.className = 'text-dark mt-1 flex-grow-1 text'
@@ -48,20 +25,18 @@ function taskinput(data){
     // document.getElementById("inputId").value = "";
 }
 
-function inp(lin){
-    var ip = document.createElement('input');
-    ip.type = 'checkbox';
-    ip.id = 'box';
-    ip.className = 'form-check-input'
-    var val = ip.value;
-    lin.appendChild(ip);
-}
-
 function icon(lin) {
     var icon = document.createElement('i');
     icon.className = 'fa fa-solid fa-2x fa-bounce fa-angles-down p-1 m-1';
     icon.style = '--fa-animation-duration: 2s; --fa-animation-iteration-count: 3;';
     lin.appendChild(icon);
+}
+
+function button(lin) {
+    var Delete = document.createElement('i');
+    Delete.className = 'fa fa-solid fa-2x fa-minus p-1 m-1';
+    lin.appendChild(Delete);
+    Delete.addEventListener('click',delSingle)
 }
 
 function image(lin) {
@@ -71,38 +46,23 @@ function image(lin) {
     lin.appendChild(clk);
 }
 
-function button(lin) {
-    var del = document.createElement('i');
-    del.className = 'fa fa-solid fa-2x fa-minus p-1 m-1';
-    lin.appendChild(del);
-    del.addEventListener('click',Delete)
-}
-
-function Delete(){
-    // e.preventDefault();
-    let propt = prompt('Are you sure?');
-    if(propt=='yes'){
-        var div = this.parentElement;
-        div.remove();
-        const d= JSON.stringify(div.firstChild.textContent);
-        del(d);
+document.getElementById('right').addEventListener('click', markRead)
+function markRead(){
+    var li = document.querySelectorAll('.li');
+    for (j=0; j<li.length;j++){
+        li[j].firstChild.style = 'text-decoration: line-through;text-decoration-style: solid;text-decoration-thickness: 2px;';
+        fetch('/complete',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                task_name:li[j].firstChild.textContent,
+            })
+        })
     }
 }
 
-function db(){
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET","/list")
-    xhr.onload = function(){
-        var dat = JSON.parse(this.response);
-        for (let i=0; i<dat.length;i++){
-            var dt = dat[i].task_name;
-            taskinput(dt)
-        }
-    }
-    xhr.send();
-}
-
-// db();
 fetchdata();       
 
 function fetchdata() {
@@ -124,19 +84,55 @@ function fetchdata() {
         .catch(error => console.error(error))
 }
 
-// function del(d) {
-//     fetch('/del', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             body: JSON.stringify(d)
-//         }
-//     })
-//         .then(res => res.json())
-//         .then(data => console.log(data))
-//         .catch(error => console.error(error));
-// }
-function del(d){
+function db(){
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET","/list")
+    xhr.onload = function(){
+        var dat = JSON.parse(this.response);
+        for (let i=0; i<dat.length;i++){
+            var dt = dat[i].task_name;
+            taskinput(dt)
+        }
+    }
+    xhr.send();
+}
+
+function delSingle(){
+    let propt = prompt('Are you sure?');
+    if(propt=='yes'){
+        var div = this.parentElement;
+        div.remove();
+        const d= JSON.stringify(div.firstChild.textContent);
+        delS(d);
+    }
+}
+
+document.getElementById('del').addEventListener('click',delAll);
+
+function delAll(){
+    let propt = prompt('Are you sure?');
+    if(propt=='yes'){
+        let lis = document.querySelectorAll('.li');
+        for (i=0; i<lis.length;i++){
+            lis[i].remove();
+            const d= JSON.stringify(lis[i].firstChild.textContent);
+            delA(d);
+        }
+    }
+}
+
+function delA(d) {
+    fetch('/del', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+            },
+        // body: JSON.stringify(d)
+        body: d
+    })
+    .catch(error => console.error(error));
+}
+function delS(d){
     var xml = new XMLHttpRequest()
     xml.open('POST','/del',true);
     xml.setRequestHeader('Content-Type','application/json')
